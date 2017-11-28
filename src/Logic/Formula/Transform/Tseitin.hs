07bc5756dba13183a117d9da285e60212f71d5cd
Matthew Wampler-Doty <matthew.wampler.doty@gmail.com>
2017-11-27 23:10:46 -0500

    Converting formulae into negation normal form

--- src/Logic/Formula/Transform/Tseitin.hs
+++ src/Logic/Formula/Transform/Tseitin.hs
@@ -6,6 +6,15 @@ import qualified Data.Set                         as Set
 import           Logic.Formula.Data.Literal
 import           Logic.Formula.Data.Propositional
 
+dualize :: Propositional a -> Propositional a
+dualize (Proposition a) = Not (Proposition a)
+dualize (Not p) = p
+dualize (x :&: y) = dualize x :|: dualize y
+dualize (x :|: y) = dualize x :&: dualize y
+dualize (x :->: y) = x :&: dualize y
+dualize Verum = Falsum
+dualize Falsum = Verum
+
 type Idx = Int
 initialIdx :: Idx
 initialIdx = 0
@@ -18,9 +27,7 @@ getFreshDefinitionalLiteral =
      return $ Definition freshVar
 
 getLastDefinitionalLiteral :: State Idx (Definitional a)
-getLastDefinitionalLiteral =
-  do lastVar <- get
-     return $ Definition lastVar
+getLastDefinitionalLiteral = fmap Definition get
 
 insert2 :: Ord a => a -> a -> Set.Set a -> Set.Set a
 insert2 x y set = Set.insert x (Set.insert y set)
@@ -34,10 +41,10 @@ set2 x y = insert2 x y Set.empty
 set3 :: Ord a => a -> a -> a -> Set.Set a
 set3 x y z = insert3 x y z Set.empty
 
-data PairClauses a = PairClauses { aDef :: a
-                                 , bDef :: a
+data PairClauses a = PairClauses { xDef :: a
+                                 , yDef :: a
                                  , fresh :: a
-                                 , abCombinedClauses :: Set.Set (Clause a)
+                                 , xyCombinedClauses :: Set.Set (Clause a)
                                  }
 
 extractPairClausesDefinitions
@@ -45,13 +52,13 @@ extractPairClausesDefinitions
   => Propositional a
   -> Propositional a
   -> State Int (PairClauses (Definitional a))
-extractPairClausesDefinitions a b =
-  do aClauses <- defineSubClause a
-     aDef <- getLastDefinitionalLiteral
-     bClauses <- defineSubClause b
-     bDef <- getLastDefinitionalLiteral
+extractPairClausesDefinitions x y =
+  do xClauses <- defineSubClause x
+     xDef <- getLastDefinitionalLiteral
+     yClauses <- defineSubClause y
+     yDef <- getLastDefinitionalLiteral
      fresh <- getFreshDefinitionalLiteral
-     return $ PairClauses aDef bDef fresh (aClauses `mappend` bClauses)
+     return $ PairClauses xDef yDef fresh (xClauses `mappend` yClauses)
 
 defineSubClause
   :: Ord a
@@ -67,35 +74,28 @@ defineSubClause (Not (Proposition a)) =
      return (set2 (set2 (Neg fresh)    (Neg (Atom a)))
                   (set2 (Pos (Atom a)) (Pos fresh)))
 
-defineSubClause (Not a) =
-  do aClauses <- defineSubClause a
-     aDef <- getLastDefinitionalLiteral
-     fresh <- getFreshDefinitionalLiteral
-     return (insert2 (set2 (Neg fresh) (Neg aDef))
-                     (set2 (Pos aDef)  (Pos fresh))
-                     aClauses)
-
+defineSubClause (Not a) = defineSubClause (dualize a)
 
 defineSubClause (a :&: b) =
   do PairClauses {..} <- extractPairClausesDefinitions a b
-     return (insert3 (set2 (Neg fresh) (Pos aDef))
-                     (set2 (Neg fresh) (Pos bDef))
-                     (set3 (Neg aDef)  (Neg bDef) (Pos fresh))
-                     abCombinedClauses)
+     return (insert3 (set2 (Neg fresh) (Pos xDef))
+                     (set2 (Neg fresh) (Pos yDef))
+                     (set3 (Neg xDef)  (Neg yDef) (Pos fresh))
+                     xyCombinedClauses)
 
 defineSubClause (a :|: b) =
   do PairClauses {..} <- extractPairClausesDefinitions a b
-     return (insert3 (set3 (Neg fresh) (Pos aDef) (Pos bDef))
-                     (set2 (Neg aDef)  (Pos fresh))
-                     (set2 (Neg bDef)  (Pos fresh))
-                     abCombinedClauses)
+     return (insert3 (set3 (Neg fresh) (Pos xDef) (Pos yDef))
+                     (set2 (Neg xDef)  (Pos fresh))
+                     (set2 (Neg yDef)  (Pos fresh))
+                     xyCombinedClauses)
 
 defineSubClause (a :->: b) =
   do PairClauses {..} <- extractPairClausesDefinitions a b
-     return (insert3 (set3 (Neg fresh) (Neg aDef) (Pos bDef))
-                     (set2 (Pos aDef)  (Pos fresh))
-                     (set2 (Neg bDef)  (Pos fresh))
-                     abCombinedClauses)
+     return (insert3 (set3 (Neg fresh) (Neg xDef) (Pos yDef))
+                     (set2 (Pos xDef)  (Pos fresh))
+                     (set2 (Neg yDef)  (Pos fresh))
+                     xyCombinedClauses)
 
 defineSubClause Verum =
   fmap ( Set.singleton . Set.singleton . Pos ) getFreshDefinitionalLiteral
@@ -118,3 +118,4 @@ definitionalClauses ::
   [Set.Set (Clause (Definitional a))]
 definitionalClauses propositions =
   evalState (mapM defineClause propositions) initialIdx
+
