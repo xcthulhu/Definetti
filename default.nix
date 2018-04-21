ab8789e3d3399bd7801da5f02da456328d2b6eea
Matthew Wampler-Doty <matthew.wampler.doty@gmail.com>
2018-04-21 08:49:27 -0400

    Updating Nix build

--- default.nix
+++ default.nix
@@ -1,2 +1,15 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc822" }:
-nixpkgs.pkgs.haskell.packages.${compiler}.callPackage ./definetti.nix {}
+{ bootstrap ? import <nixpkgs> {}, compiler ? "ghc822" }:
+
+let
+
+  nixpkgs = builtins.fromJSON (builtins.readFile ./nixpkgs.json);
+
+  src = bootstrap.fetchFromGitHub {
+    owner = "NixOS";
+    repo  = "nixpkgs";
+    inherit (nixpkgs) rev sha256;
+  };
+
+  pkgs = import src {};
+
+in pkgs.haskell.packages.${compiler}.callCabal2nix "definetti" ./. { }
