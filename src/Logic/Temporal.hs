6126f15d0dcff712287ba94c85be79efa0669303
Matthew Wampler-Doty <matthew.wampler.doty@gmail.com>
2018-07-22 19:01:16 -0400

    Temporal.hs: Converting temporal logic to normal form for deciding

--- src/Logic/Temporal.hs
+++ src/Logic/Temporal.hs
@@ -1,6 +1,40 @@
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE RankNTypes            #-}
+{-# LANGUAGE TypeOperators         #-}
+{-# LANGUAGE TypeSynonymInstances  #-}
+
 module Logic.Temporal where
 
-import           Logic.Propositional (Propositional)
+import           Control.Monad            (MonadPlus)
+import qualified Data.Set
+
+import           Logic.Propositional      (Propositional ((:&&:), Not, Verum))
+import           Logic.Propositional.DPLL (ConstrainedModelSearch,
+                                           Literal (Neg, Pos), ModelSearch,
+                                           findConstrainedModel, findModel)
+-- TODO: Timestamps
 
 
-data Temporal a = a `Until` a | Expr (Propositional a)
+data Temporal a = a `Until` a | Always a deriving (Ord, Show, Eq)
+
+type TemporalCNF a = Data.Set.Set (Data.Set.Set (Temporal a))
+
+instance ( Ord p
+         , MonadPlus m
+         , ConstrainedModelSearch d p m )
+         => ModelSearch d (TemporalCNF (Propositional p)) m where
+  findModel = undefined
+
+instance ( Ord p
+         , MonadPlus m
+         , ConstrainedModelSearch d p m )
+         => ConstrainedModelSearch d (Temporal (Propositional p)) m where
+  findConstrainedModel = findModel . Data.Set.map toTemporalDisj where
+    toTemporalDisj (Pos t) = Data.Set.singleton t
+    toTemporalDisj (Neg (Always p)) =
+        Data.Set.singleton (Verum `Until` Not p)
+    toTemporalDisj (Neg (p `Until` q)) =
+        Data.Set.fromList [ Always (Not q)
+                          , p `Until` (Not p :&&: Not q)
+                          ]
