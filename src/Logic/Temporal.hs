b8080b884600cc0ded4bd2890f85c032e5f4841e
Matthew Wampler-Doty <matthew.wampler.doty@gmail.com>
2018-08-02 07:58:13 -0400

    Temporal.hs: Using NonEmpty for models

--- src/Logic/Temporal.hs
+++ src/Logic/Temporal.hs
@@ -9,8 +9,11 @@ module Logic.Temporal (Temporal (Until, Always), before) where
 
 import           Control.Monad               (MonadPlus, msum)
 import           Data.List                   (permutations)
+import           Data.List.NonEmpty          (NonEmpty ((:|)), reverse, scanl)
 import           Data.Monoid                 (mempty, (<>))
 import qualified Data.Set
+import           Data.Traversable            (sequence)
+import           Prelude                     hiding (reverse, scanl, sequence)
 
 import           Logic.Propositional         (Propositional ((:&&:), Not, Proposition, Verum))
 import           Logic.Propositional.DPLL    (CNF, ConstrainedModelSearch,
@@ -19,8 +22,10 @@ import           Logic.Propositional.DPLL    (CNF, ConstrainedModelSearch,
 import           Logic.Propositional.Tseitin (Definitional, tseitinTransform)
 import           Logic.Semantics             (Semantics, (|=))
 
+-- | Temporal logic primitives
 data Temporal a = a `Until` a | Always a deriving (Ord, Show, Eq, Functor)
 
+-- Expression for `before` operator based on temporal logic primitives
 before :: Propositional p
        -> Propositional p
        -> Propositional (Temporal (Propositional p))
@@ -31,10 +36,11 @@ data Until_ a = a `Until_` a deriving (Functor)
 
 instance Semantics d p => Semantics [d] (Temporal p) where
   ms     |= (Always p)      = all (|= p) ms
-  (m:ms) |= u@(p `Until` q) = if m |= q
-                              then ms |= Always p
-                              else ms |= u
+  (m:ms) |= u@(p `Until` q) = m |= q || (m |= p && ms |= u)
   []     |= (_ `Until` _)   = False
+
+instance Semantics d p => Semantics (NonEmpty d) (Temporal p) where
+  (|=) (m:|ms) = (|=) (m:ms)
 
 data TimelineProblem p = TimelineProblem
   { always        :: CNF (Definitional p)
@@ -44,11 +50,11 @@ data TimelineProblem p = TimelineProblem
 instance ( Ord p
          , MonadPlus m
          , ConstrainedModelSearch d p m )
-         => ModelSearch [d] (TimelineProblem p) m where
+         => ModelSearch (NonEmpty d) (TimelineProblem p) m where
   findModel TimelineProblem {..} =
-      (fmap . fmap) snd . sequence . tail
+      fmap (reverse . fmap snd) . sequence
     $ scanl findTimeStepModel initialState untilTimeline where
-      initialState = pure (always, undefined)
+      initialState = (always,) <$> findModel always
       findTimeStepModel state (p `Until_` q) = do
         (always', _) <- state
         (always' <> p,) <$> findModel (always' <> q)
@@ -62,7 +68,7 @@ data PreTimeline p = PreTimeline
 instance ( Ord p
          , MonadPlus m
          , ConstrainedModelSearch d p m )
-         => ConstrainedModelSearch [d] (Temporal (Propositional p)) m where
+         => ConstrainedModelSearch (NonEmpty d) (Temporal (Propositional p)) m where
   findConstrainedModel clauses = searchTimelines preTimeline where
     preTimeline = foldr preproc (PreTimeline mempty [] [])
                 . Data.Set.toList
@@ -74,7 +80,7 @@ instance ( Ord p
     addTempT pt = addTemp pt . fmap tseitinTransform
     preproc (Pos p@(Always _)) pt = addTempT pt p
     preproc (Pos u@(_ `Until` _)) pt = addTempT pt u
-    preproc (Neg (Always p)) pt = addTempT pt (Verum `Until` p)
+    preproc (Neg (Always p)) pt = addTempT pt (Verum `Until` Not p)
     preproc (Neg (p `Until` q)) pt@PreTimeline{..} =
       pt { choices = (fmap . fmap) tseitinTransform
                      [p `Until` (Not p :&&: Not q), Always (Not q)]
--- test/Logic/TemporalTest.hs
+++ test/Logic/TemporalTest.hs
@@ -6,11 +6,14 @@
 module Logic.TemporalTest (temporalTests)
 where
 
+import           Data.List.NonEmpty      (NonEmpty)
+-- import           Data.Monoid             ((<>))
 import           Test.QuickCheck         (Arbitrary (arbitrary), oneof)
 import           Test.Tasty              (TestTree, testGroup)
 import           Test.Tasty.HUnit        (testCase, (@?=))
+-- import           Test.Tasty.QuickCheck   (testProperty)
 
-import           Logic.Propositional     (Propositional ((:&&:), Proposition))
+import           Logic.Propositional     (Propositional ((:&&:), Falsum, Not, Proposition, Verum))
 import           Logic.Semantics         (ModelSearch (findModel),
                                           Semantics ((|=)))
 import           Logic.Temporal          (Temporal (Always, Until), before)
@@ -21,22 +24,45 @@ import           Logic.PropositionalTest (Atom, AtomModel, bound)
 instance Arbitrary p => Arbitrary (Temporal (Propositional p)) where
   arbitrary = oneof [ Until <$> arbitrary <*> arbitrary, Always <$> arbitrary]
 
+findModel' :: Propositional (Temporal (Propositional (Atom Char)))
+           -> Maybe (NonEmpty (AtomModel Char))
+findModel' = findModel
+
 temporalLogicTests :: TestTree
 temporalLogicTests = testGroup
   "Temporal Logic Tests"
   [
-    testCase "'a before b' AND 'b before c' implies 'a before c'" $
-    (
-      (|= (a `before` c)) <$>
-      findModel' ((a `before` b) :&&: (b `before` c))) @?= Just True
+    testCase "`a before b` AND `b before c` implies `a before c`" $
+    ((|= (a `before` c)) <$>
+     findModel' ((a `before` b) :&&: (b `before` c))) @?= Just True
+  , testCase "Should not find a model for `Not (Always Verum)`" $
+    findModel' (Not (Proposition (Always Verum))) @?= Nothing
+  , testCase "Should not find a model for `Always Falsum`" $
+    findModel' (Proposition (Always Falsum)) @?= Nothing
+  , testCase "Should not find a model for `Not ((Always Verum) :&&: Verum)`" $
+    findModel' (Not (Proposition (Always Verum) :&&: Verum)) @?= Nothing
   ]
  where
   [a,b,c] = Proposition . bound <$> ['a', 'b', 'c']
-  findModel' :: Propositional (Temporal (Propositional (Atom Char)))
-             -> Maybe [AtomModel Char]
-  findModel' = findModel
+
+
+-- temporalSemanticsQC :: TestTree
+-- temporalSemanticsQC = testGroup
+--   "Temporal Semantics Laws"
+--   [ testProperty
+--         (  "Forall f: `fmap (|= f) (findModel f)"
+--         <> " == fmap (const True) (findModel f)`"
+--         )
+--       $ \f -> fmap (|= f) (findModel' f) == fmap (const True) (findModel' f)
+--   ]
+--  where
+--   findModel' :: Propositional (Temporal (Propositional (Atom Char)))
+--              -> Maybe [AtomModel Char]
+--   findModel' = findModel
 
 temporalTests :: TestTree
 temporalTests = testGroup
   "Temporal Tests"
-  [ temporalLogicTests ]
+  [ temporalLogicTests
+  -- , temporalSemanticsQC
+  ]
