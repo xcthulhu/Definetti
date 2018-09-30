45fc0f0b5c04d77127072eebea000943f5a7ee6d
Matthew Wampler-Doty <matthew.wampler.doty@gmail.com>
2018-09-30 16:39:16 -0400

    Updating nixpkgs.json, introducing glpk-hs

--- default.nix
+++ default.nix
@@ -1,3 +1,4 @@
+# Can't use ghc843 because glpk doesn't support it
 { compiler ? "ghc822" }:
 
 let
@@ -9,12 +10,18 @@ let
   };
 
   pkgs = import src {};
-
-  package = pkgs.haskell.packages.${compiler}.callCabal2nix "definetti" ./. { };
+  haskellPackages = pkgs.haskell.packages.${compiler}.override {
+    overrides = haskellPackagesNew: haskellPackagesOld: rec {
+      # TODO: Hopefully this will not be necessary when unliftio 0.2.8.1+ backports to ghc 8.2.2, or glpk supports ghc843
+      stm = haskellPackagesNew.callCabal2nix "stm" (pkgs.fetchFromGitHub{
+        owner = "haskell";
+        repo = "stm";
+        rev = "5ea70d4e15d461888866796a164bf9c177a1e8b8";
+        sha256 = "1i564qsk6p81b1f8xpv4gp75f0jwh3fcjaldsrxk7qcb0kjkynd3";
+      }) {};
+    };
+  };
 
 in
 
-  pkgs.haskell.lib.overrideCabal package (oldAttrs: {
-    # TODO: Maybe don't need this?
-    testSystemDepends = [ pkgs.glpk ];
-  })
+  haskellPackages.callCabal2nix "definetti" ./. {}
