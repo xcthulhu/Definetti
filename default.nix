95b5979d9494c33a5bbb9d0f2f62d12f56348738
Matthew Doty <matthew.wampler.doty@gmail.com>
2018-10-02 07:22:50 -0400

    Cleaning up nix overrides

--- default.nix
+++ default.nix
@@ -9,17 +9,8 @@ let
   };
 
   pkgs = import src {};
-  haskellPackages = pkgs.haskell.packages.${compiler}.override {
-    overrides = haskellPackagesNew: haskellPackagesOld: rec {
-      # TODO: Hopefully this will not be necessary when unliftio 0.2.8.1+ backports to ghc 8.2.2, or glpk supports ghc843
-      #stm = haskellPackagesNew.callCabal2nix "stm" (pkgs.fetchFromGitHub{
-      #  owner = "haskell";
-      #  repo = "stm";
-      #  rev = "5ea70d4e15d461888866796a164bf9c177a1e8b8";
-      #  sha256 = "1i564qsk6p81b1f8xpv4gp75f0jwh3fcjaldsrxk7qcb0kjkynd3";
-      #}) {};
-    };
-  };
+
+  haskellPackages = pkgs.haskell.packages.${compiler};
 
 in
 
