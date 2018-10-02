9f31baa87be68cc1af1cde06671ebb6c68866e3b
Matthew Doty <matthew.wampler.doty@gmail.com>
2018-10-02 07:19:49 -0400

    Upgrading to GHC 8.6.3

--- default.nix
+++ default.nix
@@ -1,5 +1,4 @@
-# Can't use ghc843 because glpk doesn't support it
-{ compiler ? "ghc822" }:
+{ compiler ? "ghc843" }:
 
 let
 
@@ -13,12 +12,12 @@ let
   haskellPackages = pkgs.haskell.packages.${compiler}.override {
     overrides = haskellPackagesNew: haskellPackagesOld: rec {
       # TODO: Hopefully this will not be necessary when unliftio 0.2.8.1+ backports to ghc 8.2.2, or glpk supports ghc843
-      stm = haskellPackagesNew.callCabal2nix "stm" (pkgs.fetchFromGitHub{
-        owner = "haskell";
-        repo = "stm";
-        rev = "5ea70d4e15d461888866796a164bf9c177a1e8b8";
-        sha256 = "1i564qsk6p81b1f8xpv4gp75f0jwh3fcjaldsrxk7qcb0kjkynd3";
-      }) {};
+      #stm = haskellPackagesNew.callCabal2nix "stm" (pkgs.fetchFromGitHub{
+      #  owner = "haskell";
+      #  repo = "stm";
+      #  rev = "5ea70d4e15d461888866796a164bf9c177a1e8b8";
+      #  sha256 = "1i564qsk6p81b1f8xpv4gp75f0jwh3fcjaldsrxk7qcb0kjkynd3";
+      #}) {};
     };
   };
 
