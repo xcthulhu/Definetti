ce04b8249f4af565d00b73456c3a2ca7721caa8b
Matthew Doty <matthew.wampler.doty@gmail.com>
2018-10-04 08:45:14 -0400

    shell.nix: Introducing ghcid for continuous testing

--- shell.nix
+++ shell.nix
@@ -1,1 +1,10 @@
-(import ./default.nix {}).env
+{pkgs ? import <nixpkgs> {}}:
+let
+  package = pkgs.haskell.lib.overrideCabal (import ./default.nix {}) (oldAttrs: {
+    testSystemDepends = let oldTestSystemDepends = if oldAttrs ? testSystemDepends
+                                                   then oldAttrs.testSystemDepends
+                                                   else [];
+                        in [ pkgs.haskellPackages.ghcid ] ++ oldTestSystemDepends;
+  });
+in
+  package.env
