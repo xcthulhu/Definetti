19f3861f4299ac325c370c9130d24a27a30ef586
Matthew Doty <matthew.wampler.doty@gmail.com>
2018-10-30 09:07:08 -0400

    LinearProgrammingTest.hs: Added test, changed names to be more consistent with other tests.

--- test/Logic/Tasty/Suite/LinearProgrammingTest.hs
+++ test/Logic/Tasty/Suite/LinearProgrammingTest.hs
@@ -15,7 +15,7 @@ import           Control.Monad.Trans.Maybe (runMaybeT)
 import qualified Data.Map                  as Map
 import           Data.Ratio                ((%))
 import qualified Data.Set                  as Set
-import           Logic.LinearProgramming   (LinearInequality ((:<=:)),
+import           Logic.LinearProgramming   (LinearInequality ((:<:), (:<=:)),
                                             SumPlusConstant ((:+:)))
 
 
@@ -34,17 +34,34 @@ findConstrainedModel' = runMaybeT . findConstrainedModel
 
 linearProgrammingTests :: TestTree
 linearProgrammingTests = testGroup
-  "Basic Tests"
-  [ testCase "Can find a model for 1/2 X <= X" $ do
+  "Linear programming tests"
+  [ testCase "Exists a model s.t. 1/2 X <= X" $ do
       let inequality = [(1 % 2, "X")] :+: 0 :<=: [(1, "X")] :+: 0
       model <- findConstrainedModel' $ Set.fromList [Pos inequality]
       (|= inequality) <$> model @?= Just True
-  , testCase "Can find a model for X == 0" $ do
+
+  , testCase "Exists a model s.t. X == 0" $ do
       let lt0 = [(1, "X")] :+: 0 :<=: [] :+: 0
-      let gt0 = [] :+: 0 :<=: [(1, "X")] :+: 0
-      let gt1 = [] :+: 1 :<=: [(1 :: Rational, "X")] :+: 0
+          gt0 = [] :+: 0 :<=: [(1, "X")] :+: 0
+          gt1 = [] :+: 1 :<=: [(1 :: Rational, "X")] :+: 0
       model <- findConstrainedModel' $ Set.fromList [ Pos lt0, Pos gt0 ]
       (|= lt0) <$> model @?= Just True
       (|= gt0) <$> model @?= Just True
       (|= gt1) <$> model @?= Just False
+
+  , testCase "Exists a model s.t. 2 X <= X" $ do
+      let inequality = [(2, "X")] :+: 0 :<=: [(1, "X")] :+: 0
+          gt0 = [] :+: 0 :<: [(1, "X")] :+: (0 :: Rational)
+      model <- findConstrainedModel' $ Set.fromList [ Pos inequality ]
+      (|= inequality) <$> model @?= Just True
+      (|= gt0) <$> model @?= Just False
+
+  , testCase "Exists a model s.t. A < B && B < C" $ do
+      let aLTb = [(1, "A")] :+: 0 :<: [(1, "B")] :+: 0
+          bLTc = [(1, "B")] :+: 0 :<: [(1, "C")] :+: 0
+          aLTc = [(1, "A")] :+: 0 :<: [(1, "C")] :+: (0 :: Rational)
+          cLEa = [(1, "C")] :+: 0 :<=: [(1, "A")] :+: (0 :: Rational)
+      model <- findConstrainedModel' $ Set.fromList [ Pos aLTb, Pos bLTc ]
+      (|= aLTc) <$> model @?= Just True
+      (|= cLEa) <$> model @?= Just False
   ]
