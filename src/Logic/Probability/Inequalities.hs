47f36676a936a939eb09e90df7e91adba08870c7
Matthew Doty <matthew.wampler.doty@gmail.com>
2019-05-05 08:59:42 -0400

    Refactoring intWeightedMaxSatGT back into Logic.Probabilities.Inequalities

--- src/Logic/Probability/Inequalities.hs
+++ src/Logic/Probability/Inequalities.hs
@@ -1,4 +1,6 @@
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE MonoLocalBinds #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE NamedFieldPuns #-}
 
@@ -14,15 +16,17 @@ import Data.Monoid ((<>))
 import Data.Ratio (denominator)
 import Data.Tuple (swap)
 
+import Control.Applicative (Alternative((<|>), empty))
+import Data.Foldable (asum, fold)
+import GHC.Exts (sortWith)
 import Logic.Probability.Categorical
   ( Categorical
   , Probability((:*), (:+), (:-), Const, Pr)
   , pr
   )
 import Logic.Propositional (Propositional(Not))
-import Logic.Propositional.Internal.DPLL (ConstrainedModelSearch)
+import Logic.Propositional.Internal.DPLL (CNF, ConstrainedModelSearch)
 import Logic.Propositional.Internal.Tseitin (tseitinTransform)
-import Logic.Propositional.Internal.WeightedSat (intWeightedSatGT)
 import Logic.Semantics (ModelSearch(findModel), Semantics((|=)))
 
 -- | Probability Inequalities
@@ -89,7 +93,7 @@ normalizeProbInequality (a :<= b) =
     }
   where
     weightedTerms =
-      fmap swap . Map.toList $
+      filter ((/= 0) . fst) . fmap swap . Map.toList $
       extractPropositions a `minus` extractPropositions b
     (lhts, rhts) =
       second (fmap (first negate)) .
@@ -104,6 +108,66 @@ unNormalizeProbInequality gnf
     toProb lhConst lht :<= toProb 0 rht
   where
     toProb c terms = foldr (:+) (Const c) $ (\(k, p) -> k :* Pr p) <$> terms
+
+-- | Choose elements of a weighted collection with collective weight
+--   greater than or equal to `k` such that if any element was removed
+--   the collection would weigh less than `k`.
+--
+--   If all elements have weight 1, then
+--   `|totalWeight choose k| = totalWeight! / (k!(totalWeight-k)!)`
+--
+--   Lifted into an arbitrary `Alternative` functor;
+--   using `List` results in a list of all of the possibilities.
+weightedChoose :: Alternative f => Integer -> [(Integer, a)] -> f [a]
+weightedChoose k xs = weightedChoose' k sortedXs (sum . fmap fst $ sortedXs)
+  where
+    sortedXs = sortWith fst xs
+    weightedChoose' ::
+         Alternative f => Integer -> [(Integer, a)] -> Integer -> f [a]
+    weightedChoose' c clauses totalWeight
+      | c > totalWeight = empty
+      | c <= 0 = pure []
+      | [] <- clauses = error "Pattern should be unreachable!"
+      | ((weight, x):clauses') <- clauses =
+        let totalWeight' = totalWeight - weight
+         in fmap (x :) (weightedChoose' (c - weight) clauses' totalWeight') <|>
+            weightedChoose' c clauses' totalWeight'
+
+-- | Find a model of positive integer weighted clauses in
+--   conjunctive normal form with weight greater or equal `k`
+--
+--   In particular we have the rule:
+--
+--   k ≤ |intMaxSAT weightedClauses| ≡ intWeightedSatGTE k weightedClauses
+--
+--   The right hand side is thought to be more tractable than the left hand side.
+--
+--   The left hand side is in MAXSAT.
+--   The right hand side is in NP.
+--   It is believed NP ⊊ MAXSAT.
+intWeightedSatGTE ::
+     (Ord a, Alternative m, ModelSearch d (CNF a) m)
+  => Integer
+  -> [(Integer, CNF a)]
+  -> m d
+intWeightedSatGTE k weightedClauses =
+  asum . fmap (findModel . fold) $ chooseN weightedClauses
+  where
+    chooseN :: [(Integer, CNF a)] -> [[CNF a]]
+    chooseN = weightedChoose k
+
+-- | Find a model of positive integer weighted clauses in
+--   conjunctive normal form with weight greater than `k`
+--
+--   We have the rule:
+--
+--   k < |intMaxSAT weightedClauses| ≡ intWeightedSatGT k weightedClauses
+intWeightedSatGT ::
+     (Ord a, Alternative m, ModelSearch d (CNF a) m)
+  => Integer
+  -> [(Integer, CNF a)]
+  -> m d
+intWeightedSatGT k = intWeightedSatGTE (k + 1)
 
 instance Semantics d p =>
          Semantics (Categorical d) (GTSummationNormalForm p) where
